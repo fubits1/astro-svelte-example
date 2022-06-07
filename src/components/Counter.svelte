0c49495ac1a4fa99d1fe03ae7a0312c485f3f1bb
fubits <ilja_s@protonmail.com>
2022-06-08 00:33:08 +0200

    init: svelte + astro

---
+++ src/components/Counter.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { state } from "../state.js";
+  import { fade } from "svelte/transition";
+  $: open = $state;
+  let count = 0;
+
+  function add() {
+    count += 1;
+  }
+
+  function subtract() {
+    count -= 1;
+  }
+</script>
+
+{#if open}
+  <div class="counter" transition:fade>
+    <button on:click={subtract}>-</button>
+    <pre>{count}</pre>
+    <button on:click={add}>+</button>
+  </div>
+  <div class="message" transition:fade>
+    <slot />
+  </div>
+{/if}
+
+<style>
+  .counter {
+    display: grid;
+    font-size: 2em;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    margin-top: 2em;
+    place-items: center;
+  }
+  .message {
+    text-align: center;
+  }
+</style>
---
+++ src/sections/Hero.svelte
@@ -0,0 +1,31 @@
+<script>
+  // export let state;
+  import { state } from "../state.js";
+  function updateState() {
+    $state = !$state;
+  }
+</script>
+
+<section>
+  <h1>Hero</h1>
+  <button on:click={updateState}>Start ({$state})</button>
+</section>
+
+<style>
+  section {
+    height: 50vh;
+    width: 100%;
+    background: linear-gradient(to right, #fdfbfb, #ebedee);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
+
+  button {
+    position: absolute;
+    bottom: 10%;
+    border-radius: 8px;
+  }
+</style>
