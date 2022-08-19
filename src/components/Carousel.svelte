a9956367bbe2035c8cefe5d34718838cf5e87b52
fubits <ilja_s@protonmail.com>
2022-08-19 12:34:19 +0200

    chore: astro update; switched to pnpm

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,20 @@
+<script></script>
+
+<section>
+  <div id="container">
+    <code>TODO</code>
+  </div>
+</section>
+
+<style>
+  #container {
+    width: 100vw;
+    height: 100vh;
+    padding: 1rem;
+  }
+  @media only screen and (max-width: 800px) {
+    #container {
+      padding: 0.5rem;
+    }
+  }
+</style>
